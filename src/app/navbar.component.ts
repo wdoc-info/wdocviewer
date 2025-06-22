9fc5427a68fe2bcc48ea09c93e8058fbfd7aa1bc
cyrilbele <47146282+cyrilbele@users.noreply.github.com>
2025-06-22 18:23:09 +0200

    feat: add form saving support

--- src/app/navbar.component.ts
+++ src/app/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -10,11 +10,17 @@ import { CommonModule } from '@angular/common';
 })
 export class NavbarComponent {
   @Output() fileSelected = new EventEmitter<File>();
+  @Output() save = new EventEmitter<void>();
+  @Input() showSave = false;
 
   onFileChange(event: Event) {
     const input = event.target as HTMLInputElement;
     if (input.files && input.files.length > 0) {
       this.fileSelected.emit(input.files[0]);
     }
+  }
+
+  onSave() {
+    this.save.emit();
   }
 }
