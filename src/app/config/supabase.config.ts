85621477326fd231a10bb12b9d825fad3d7404ca
cyrilbele <47146282+cyrilbele@users.noreply.github.com>
2025-12-02 12:07:05 +0100

    Inline app version into app config

--- src/app/config/supabase.config.ts
+++
@@ -1,9 +0,0 @@
-export const supabaseConfig = {
-  url: 'https://wqieplruslupjlxfpeqq.supabase.co',
-  anonKey:
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndxaWVwbHJ1c2x1cGpseGZwZXFxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQwNDAwOTQsImV4cCI6MjA3OTYxNjA5NH0.RsR3rjdOa5KYFh5ew-5l4AZjQxxgXEpb4m9tY7da6po',
-  redirectUrls: {
-    local: 'http://localhost:4200',
-    production: 'https://app.wdoc.info',
-  },
-};
